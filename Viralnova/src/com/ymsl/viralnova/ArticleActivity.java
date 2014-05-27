bd2910436b2b7ada18f429c2cf058bda7d556eec
ellyxu <18612452814@163.com>
2014-05-27 16:31:02 +0800

    Version1.0
    show correct contents.

--- Viralnova/src/com/ymsl/viralnova/ArticleActivity.java
+++ Viralnova/src/com/ymsl/viralnova/ArticleActivity.java
@@ -1,12 +1,5 @@
 package com.ymsl.viralnova;
 
-import java.util.ArrayList;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import com.alibaba.fastjson.JSON;
-
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
@@ -61,53 +54,40 @@ class WebAppInterface {
 	
 	@JavascriptInterface
 	public String getContent() {
+		String data = mArticle.getContent();
+		System.out.println(data);
+		return data;
+	}
+	
+	@JavascriptInterface
+	public String getContent2() {
 		final String data = "{" +
 				"list: [" +
-						"{'type':'text', 'url':null, 'pre':null, 'post':null, 'text':'Humans, in general, have an obsession with the future. " +
+						"{'type':'text', 'text':'Humans, in general, have an obsession with the future. " +
 						"We worry about what we will be doing in 5 years, what technological advancements will be and what the world will look " +
 						"like for our children. We have always been that way.<br/><br/>Take these ads from past decades as an example. The vintage " +
 						"advertisements attempted to guess what the future would look like. Sometimes they were close�� but most often, they were " +
 						"way, way off.'}," +
 						"{'type':'image', 'url':'http://content3.viralnova.com/wp-content/uploads/2014/05/vintage-ads-future.jpg', " +
-						"'pre':'1.) They thought that THIS is what voicemail would look like.'," +
-						"'post':'Huff Post<br/><br/>'," +
-						"'text':null}," +
+						"'pre':'1.) They thought that THIS is what voicemail would look like.'}," +
 						"{'type':'image', 'url':'http://content4.viralnova.com/wp-content/uploads/2014/05/vintage-ads-future2.jpg', " +
-						"'pre':'2.) Instead of a portable AC unit, maybe just air condition the entire vehicle?'," +
-						"'post':'Huff Post<br/><br/>'," +
-						"'text':null}," +
+						"'pre':'2.) Instead of a portable AC unit, maybe just air condition the entire vehicle?'}," +
 						"{'type':'image', 'url':'http://content4.viralnova.com/wp-content/uploads/2014/05/vintage-ads-future3.jpg', " +
-						"'pre':'3.) Instead of automatic doors, there was�� this?'," +
-						"'post':'Huff Post<br/><br/>'," +
-						"'text':null}," +
+						"'pre':'3.) Instead of automatic doors, there was�� this?'}," +
 						"{'type':'image', 'url':'http://content4.viralnova.com/wp-content/uploads/2014/05/vintage-ads-future4.jpg', " +
-						"'pre':'4.) Not sure if we��ll ever get to the Jetson flying car like this.'," +
-						"'post':'Huff Post<br/><br/>'," +
-						"'text':null}," +
+						"'pre':'4.) Not sure if we��ll ever get to the Jetson flying car like this.'}," +
 						"{'type':'image', 'url':'http://content4.viralnova.com/wp-content/uploads/2014/05/vintage-ads-future5.jpg', " +
-						"'pre':'5.) Self-driving cars? Yes, please.'," +
-						"'post':'Huff Post<br/><br/>'," +
-						"'text':null}," +
+						"'pre':'5.) Self-driving cars? Yes, please.'}," +
 						"{'type':'image', 'url':'http://content4.viralnova.com/wp-content/uploads/2014/05/vintage-ads-future6.jpg', " +
-						"'pre':'6.) Self-checkins are thankfully not this complicated at the airport. Most of the time.'," +
-						"'post':'Huff Post<br/><br/>'," +
-						"'text':null}," +
+						"'pre':'6.) Self-checkins are thankfully not this complicated at the airport. Most of the time.'}," +
 						"{'type':'image', 'url':'http://content4.viralnova.com/wp-content/uploads/2014/05/vintage-ads-future7.jpg', " +
-						"'pre':'7.) Getting things delivered to you isn��t that outlandish.'," +
-						"'post':'Huff Post<br/><br/>'," +
-						"'text':null}," +
+						"'pre':'7.) Getting things delivered to you isn��t that outlandish.'}," +
 						"{'type':'image', 'url':'http://content4.viralnova.com/wp-content/uploads/2014/05/vintage-ads-future8.jpg', " +
-						"'pre':'8.) They were right. Food trucks ARE awesome.'," +
-						"'post':'Huff Post<br/><br/>'," +
-						"'text':null}," +
+						"'pre':'8.) They were right. Food trucks ARE awesome.'}," +
 						"{'type':'image', 'url':'http://content4.viralnova.com/wp-content/uploads/2014/05/vintage-ads-future9.jpg', " +
-						"'pre':'9.) This HAS to happen.'," +
-						"'post':'Huff Post<br/><br/>'," +
-						"'text':null}," +
+						"'pre':'9.) This HAS to happen.'}," +
 						"{'type':'image', 'url':'http://content4.viralnova.com/wp-content/uploads/2014/05/vintage-ads-future10.jpg', " +
-						"'pre':'10.) Because I never want to fold clothes ever again.'," +
-						"'post':'Huff Post<br/><br/>'," +
-						"'text':null}," +
+						"'pre':'10.) Because I never want to fold clothes ever again.'}," +
 					"]}";
 		Log.d(TAG, data);
 		return data;
